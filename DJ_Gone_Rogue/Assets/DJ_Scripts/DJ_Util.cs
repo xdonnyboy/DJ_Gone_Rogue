c60215713dc95759809248447e302c308013c926
cptcaveman <wsndrs@gmail.com>
2014-01-17 19:32:48 -0800

    adding a static util class. currently has a function to get the tile pos of an object

---
+++ DJ_Gone_Rogue/Assets/DJ_Scripts/DJ_Util.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public static class DJ_Util
+{
+	/// <summary>
+	/// Gets the tile position based on the actual position.
+	/// </summary>
+	/// <param name="position">Position.</param>
+	/// <param name="tilePos">Tile position.</param>
+	public static void GetTilePos(Vector3 position, ref DJ_Point tilePos)
+	{
+		tilePos.X = (int)(position.x + .5f);
+		tilePos.Y = (int)(position.z + .5f);
+	}
+}
