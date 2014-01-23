70a36f8dbe0bdd10db78886ad2f2356c6e7def75
Fernando Carrillo <fcarril1@ucsc.edu>
2014-01-23 04:00:22 -0800

    Fixed some more prefab errors that I found

--- DJ_Gone_Rogue/Assets/DJ_Scripts/DJ_TileManagerScript.cs
+++ DJ_Gone_Rogue/Assets/DJ_Scripts/DJ_TileManagerScript.cs
@@ -9,10 +9,11 @@ using System.Collections.Generic;
 /// responsible for interfacing with other managers.
 /// 
 /// @author - Wyatt Sanders 1/9/2014
+/// @edited - Fernando Carrillo 1/23/2014
 /// </summary>
 public class DJ_TileManagerScript : DJ_ManagerScript
 {
-
+	
 	// Use this for initialization
 	public override void Start ()
 	{
@@ -21,13 +22,15 @@ public class DJ_TileManagerScript : DJ_ManagerScript
 		//this is only going tobe attached to an empty object but center
 		//it at the origin anyway
 		gameObject.transform.position = Vector3.zero;
-
+		
 		tilePool = new ArrayList();
 		tileMap = new Dictionary<DJ_Point, GameObject>();
 		closestTiles = new Dictionary<DJ_Point, GameObject>();
 		
-		FromFile("");
-
+		// change to access information from the LevelManager.
+		//FromFile("");
+		instantiateTiles();
+		
 		// The closestTiles should only be the tiles nearby to the player
 		// But right now, it is all the tiles in the level a.k.a the TileMap
 		closestTiles = tileMap;
@@ -46,17 +49,17 @@ public class DJ_TileManagerScript : DJ_ManagerScript
 			getTilesNearLocation (playerTilePos);
 		}
 	}
-
+	
 	public static void ReceiveBroadcast(ref DJ_Broadcast broadcast)
 	{
-
+		
 	}
-
+	
 	public static void SendBroadcast()
 	{
 	}
-
-	public override void FromFile (string path)
+	
+	/*public override void FromFile (string path)
 	{
 		base.FromFile (path);
 
@@ -64,48 +67,31 @@ public class DJ_TileManagerScript : DJ_ManagerScript
 
 		makeGrid (_num);
 	}
-
+	*/
+	
 	public override void Dispose ()
 	{
 		base.Dispose ();
 	}
-
+	
 	/// <summary>
 	/// Makes an n x n grid of tiles.
 	/// </summary>
 	/// <param name="_num">_num.</param>
-	void makeGrid (int n)
+	void instantiateTiles()
 	{
 		Vector3 _p = Vector3.zero;
 		
-		for (int x = 0; x < n; x++)
-		{
-			_p.z = 0;
-			for (int z = 0; z < n; z++)
-			{
-				//Create the tile object and tag it as a GameObject
-				GameObject _t = (GameObject.Instantiate (m_tilePrefab)) as GameObject;
-				
-				_t.transform.parent = transform;
-				
-				//set the position of the tile
-				_t.transform.position = _p;
-				
-				//Add the tile to our list of active tiles
-				DJ_Point newPoint = new DJ_Point((int)(_p.x + .5f), (int)(_p.z + .5f));
-				
-				//print ("adding tile at " + newPoint);
-				
-				tileMap.Add (newPoint, _t);
-				
-				//increment the z portion of the position
-				_p.z += (m_tilePrefab.transform.GetChild (0).GetComponent (typeof(MeshRenderer)) as MeshRenderer).bounds.size.z;
-			}
-			//increment the x portion of the position
-			_p.x += (m_tilePrefab.transform.GetChild (0).GetComponent (typeof(MeshRenderer)) as MeshRenderer).bounds.size.x;
+		Debug.Log("In TileManager accessing " + DJ_LevelParser.tileList.Count + " amount of tiles from DJ_LevelParser");
+		foreach( Tile tile in DJ_LevelParser.tileList){
+			GameObject _t = (GameObject.Instantiate(m_tilePrefab)) as GameObject;
+			_t.transform.parent = transform;
+			_t.transform.position = new Vector3(tile.x, 0, tile.z);
+			DJ_Point newPoint = new DJ_Point((int)(tile.x + .5f), (int)(tile.z + .5f));
+			tileMap.Add(newPoint, _t);
 		}
 	}
-
+	
 	void getTilesNearLocation (DJ_Point playerTilePos)
 	{
 		//if the player has moved tiles, we need to update the closest ones
@@ -119,11 +105,11 @@ public class DJ_TileManagerScript : DJ_ManagerScript
 			
 			DJ_PlayerManager.prevPlayerTilePos.X = playerTilePos.X;
 			DJ_PlayerManager.prevPlayerTilePos.Y = playerTilePos.Y;
-
+			
 			//now get a ref to  the tiles around the  player
 			//getNearbyTiles (playerTilePos, nearbyTiles, 1.0f);
 			getNearbyTiles(playerTilePos, 1.0f);
-
+			
 			//Debugging view to see if tiles update correctly
 			//			for (int i = 0; i < nearbyTiles.Count; ++i) {
 			//				DJ_Point p = nearbyTiles [i];
@@ -163,30 +149,30 @@ public class DJ_TileManagerScript : DJ_ManagerScript
 			}
 		}
 	}
-
+	
 	float time = 0.0f;
 	float beatTime = 2.0f;
 	bool inEditorMode = false;
-
+	
 	private List<DJ_Point> nearbyTiles = new List<DJ_Point>();
-
+	
 	/// <summary>
 	/// The tile prefab.
 	/// </summary>
 	public GameObject m_tilePrefab;
-
+	
 	private Texture[] m_tileTextures;
-
+	
 	/// <summary>
 	/// The list of pooled tiles.
 	/// </summary>
 	public static ArrayList tilePool;
-
+	
 	/// <summary>
 	/// The tile map. A dictionary of all the tiles in the level.
 	/// </summary>
 	public static Dictionary<DJ_Point, GameObject> tileMap;
-
+	
 	/// <summary>
 	/// The closest tiles. A dictionary of the closest tiles to the player on the level.
 	/// </summary>
